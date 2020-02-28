0c2020b522d7a7577d35d5a8b49b07a8cc8f9fca
Mélanie <mlanie.parry@gmail.com>
2020-02-28 18:24:47 +0100

    fonctionnalité cocktailAleatoire terminée

--- monbartender/src/components/accueil/Accueil.js
+++ monbartender/src/components/accueil/Accueil.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+
+import "./Accueil.css";
+
+// eslint-disable-next-line no-undef
+const apiBaseURL = process.env.REACT_APP_BASE_API;
+
+const initialState = {
+  nom: "",
+  photo: ""
+};
 
 const Accueil = () => {
-  return <div className="pageaccueil"> Je suis la page d&apos;Accueil</div>;
+  const [cocktail, setCocktail] = useState(initialState);
+
+  const getCocktailAleatoiredata = () => {
+    fetch(`${apiBaseURL}/api/cocktails/aleatoire`)
+      .then(reponse => {
+        return reponse.json();
+      })
+      .then(data => {
+        setCocktail(data);
+      })
+      .catch(error => {
+        console.log("vous avez une erreur : ", error);
+      });
+  };
+
+  React.useEffect(() => {
+    getCocktailAleatoiredata();
+  }, []);
+
+  console.log("cocktail : ", cocktail);
+
+  return (
+    <>
+      <h2>Cocktails du moment</h2>
+      <img
+        className="imageRecetteAleatoire"
+        src={`${apiBaseURL}${cocktail.photo}`}
+        alt={cocktail.nom}
+      />
+      <div className="nomcocktail">{cocktail.nom}</div>
+
+    </>
+  );
 };
 
 export default Accueil;
