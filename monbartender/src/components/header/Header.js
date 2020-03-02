bc9d2cc9ede4222a3eb61419fecae067230f3168
Mélanie <mlanie.parry@gmail.com>
2020-03-02 12:51:16 +0100

    début de la fonctionnalité autehentification

--- monbartender/src/components/header/Header.js
+++ monbartender/src/components/header/Header.js
@@ -5,20 +5,27 @@ import { FaUserAlt } from "react-icons/fa";
 import "./Header.css";
 
 const Header = () => {
+  const connexion = () => {
+    console.log("on a cliqué sur le bouton de connexion");
+
+
+  };
+
+  
   const modifierCSSNavBarre = () => {
     const divNavBar = document.getElementsByClassName("navbar");
-    
+
     const divTxtaccueil = document.getElementsByClassName("txtaccueil");
     const divTxtRecherche = document.getElementsByClassName("txtRecherche");
     const divTxtRecettes = document.getElementsByClassName("txtRecettes");
-    
+
     const divTxtProfil = document.getElementsByClassName("txtProfil");
     const divTxtBar = document.getElementsByClassName("txtBar");
     const divTxtFavoris = document.getElementsByClassName("txtFavoris");
-   
+
     if (divNavBar[0].classList.value === "navbar") {
       divNavBar[0].classList.add("navBarRetreci");
-      
+
       divTxtaccueil[0].classList.add("divInvisible");
       divTxtRecherche[0].classList.add("divInvisible");
       divTxtRecettes[0].classList.add("divInvisible");
@@ -26,10 +33,9 @@ const Header = () => {
       divTxtProfil[0].classList.add("divInvisible");
       divTxtBar[0].classList.add("divInvisible");
       divTxtFavoris[0].classList.add("divInvisible");
-
     } else {
       divNavBar[0].classList.remove("navBarRetreci");
-      
+
       divTxtaccueil[0].classList.remove("divInvisible");
       divTxtRecherche[0].classList.remove("divInvisible");
       divTxtRecettes[0].classList.remove("divInvisible");
@@ -37,7 +43,6 @@ const Header = () => {
       divTxtProfil[0].classList.remove("divInvisible");
       divTxtBar[0].classList.remove("divInvisible");
       divTxtFavoris[0].classList.remove("divInvisible");
-
     }
   };
 
@@ -62,7 +67,12 @@ const Header = () => {
           placeholder="nom du cocktail"
         />
       </div>
-      <div className="iconLogin">
+      <div
+        className="iconLogin"
+        onClick={() => {
+          connexion();
+        }}
+      >
         <FaUserAlt size={44} />
       </div>
     </div>
