ae1914cfc2c9d04ec1ec055a73008ce163f3e8ef
Mélanie <mlanie.parry@gmail.com>
2020-06-25 15:09:49 +0200

    modif composant pour recherche par ing

--- monbartender/src/components/rechercheParIngredient/RechercheParIngredient.js
+++ monbartender/src/components/rechercheParIngredient/RechercheParIngredient.js
@@ -1,19 +1,95 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../../context/authContext";
-import RechercheParIngredientConnecte from "../rechercheParIngredientConnecte/RechercheParIngredientConnecte";
-import RechercheParIngredientNonConnecte from "../rechercheParIngredientNonConnecté/RechercheParIngredientNonConnecte";
+import Axios from "axios";
+
+import "./RechercheParIngredient.css";
+import "./RechercheParIngredientDesktop.css";
+
+import SelectComponentAllIngredients from "../selectComponentAllIngredients/SelectComponentAllIngredients";
+import ListeCocktailsComponent from "../listeCocktailsComponent/ListeCocktailsComponent";
+import SelectComponentIngredientsBar from "../selectComponentIngredientsBar/SelectComponentIngredientsBar";
+
+// eslint-disable-next-line no-undef
+const apiBaseURL = process.env.REACT_APP_BASE_API;
 
 const RechercheParIngredient = () => {
   const { user } = useContext(AuthContext);
+  const [cocktailsRecherche, setCocktailsRecherche] = useState();
 
-  return user ? (
-    <>
-      <RechercheParIngredientConnecte />
-    </>
-  ) : (
-    <>
-      <RechercheParIngredientNonConnecte />
-    </>
+  const Recherche = event => {
+    event.preventDefault();
+
+    let nomIngredientdivSelect1 = document.getElementById("divSelect1").value;
+    if (nomIngredientdivSelect1 === "choisissez un ingredient")
+      nomIngredientdivSelect1 = "";
+
+    let nomIngredientdivSelect2 = document.getElementById("divSelect2").value;
+    if (nomIngredientdivSelect2 === "choisissez un ingredient")
+      nomIngredientdivSelect2 = "";
+
+    let nomIngredientdivSelect3 = document.getElementById("divSelect3").value;
+    if (nomIngredientdivSelect3 === "choisissez un ingredient")
+      nomIngredientdivSelect3 = "";
+
+    if (
+      nomIngredientdivSelect1 === "" &&
+      nomIngredientdivSelect2 === "" &&
+      nomIngredientdivSelect3 === ""
+    ) {
+      Axios.get(`${apiBaseURL}/api/v1/cocktails`)
+        .then(reponse => {
+          setCocktailsRecherche(reponse.data);
+        })
+        .catch(error => {
+          console.log("vous avez une erreur : ", error);
+        });
+    } else {
+      Axios.get(
+        `${apiBaseURL}/api/v1/cocktails/rechercher-par-ingredient?ingredient1=${nomIngredientdivSelect1}&ingredient2=${nomIngredientdivSelect2}&ingredient3=${nomIngredientdivSelect3}`
+      )
+        .then(reponse => {
+          setCocktailsRecherche(reponse.data);
+        })
+        .catch(error => {
+          console.log("vous avez une erreur : ", error);
+        });
+    }
+  };
+
+  return (
+    <div id="page-recherche">
+      <div id="titre-recherche-ingredient">Recherche par ingredients</div>
+      <div id="recherche">
+        <div id="choix-ingredients">
+          <div id="titre-ingredient-cocktail">Ingredients du cocktail</div>
+          {user ? (
+            <div id="selection-ingredient">
+              <SelectComponentIngredientsBar id="divSelect1" />
+              <SelectComponentIngredientsBar id="divSelect2" />
+              <SelectComponentIngredientsBar id="divSelect3" />
+            </div>
+          ) : (
+            <div id="selection-ingredient">
+              <SelectComponentAllIngredients id="divSelect1" />
+              <SelectComponentAllIngredients id="divSelect2" />
+              <SelectComponentAllIngredients id="divSelect3" />
+            </div>
+          )}
+        </div>
+        <div id="div-btn-recherche">
+          <button id="btn-recherche-par-ingredient" onClick={Recherche}>
+            Lancer la recherche ...
+          </button>
+        </div>
+      </div>
+
+      {cocktailsRecherche && (
+        <div id="resultat-recherche">
+          <div id="titre-resultat-recherche">Résultat de votre recherche</div>
+          <ListeCocktailsComponent cocktails={cocktailsRecherche} />
+        </div>
+      )}
+    </div>
   );
 };
 
