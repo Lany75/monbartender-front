49e1bb723a761060527857561c8446f8b1e18845
Mélanie <mlanie.parry@gmail.com>
2020-07-31 12:08:32 +0200

    affichage liste des verres

--- monbartender/src/components/gestionVerres/GestionVerres.js
+++ monbartender/src/components/gestionVerres/GestionVerres.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { VerreContext } from "../../context/verreContext";
 
 import "./GestionVerres.css";
 import "./GestionVerresDesktop.css";
 
-const gestionVerres = () => {
+const GestionVerres = () => {
+  const { listeVerres } = useContext(VerreContext);
+
   return (
     <div id="gestion-verres">
       <div id="titre-btn-verres">
         <div id="titre-ajout-verres">Liste verres</div>
         <button id="btn-ajouter-verres">Ajouter</button>
       </div>
-      <div id="items-verres"></div>
+      <div id="items-verres">
+        {listeVerres &&
+          listeVerres.map((lv, index) => {
+            return (
+              <div id="nom-verre-gestion" key={index}>
+                {lv.nom}
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
 
-export default gestionVerres;
+export default GestionVerres;
