5ba313781da752165ec2d77192c28b2854b80844
Lany75 <mlanie.parry@gmail.com>
2021-06-09 14:32:17 +0200

    F2 - Creation composant AppliTitre

---
+++ monbartender/src/components/appliTitre/AppliTitre.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './AppliTitre.css';
+import './AppliTitreDesktop.css';
+
+const AppliTitre = () => {
+  return <div className="appli-titre">Mon BarTender</div>
+}
+
+export default AppliTitre
