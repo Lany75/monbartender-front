08150eac9c3372f0e35437a5d64ef812586322f3
Lany75 <mlanie.parry@gmail.com>
2021-06-09 16:00:33 +0200

    F2 - Ajout bouton Menu dans header

--- monbartender/src/components/header/Header.js
+++ monbartender/src/components/header/Header.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import AuthButton from "../authButton/AuthButton";
 import AppliTitre from "../appliTitre/AppliTitre";
+import MenuButton from "../menuButton/MenuButton";
 
 import "./Header.css";
 
 const Header = () => {
   return (
     <div className="header">
+      <MenuButton />
       <AppliTitre />
       <AuthButton />
     </div>
