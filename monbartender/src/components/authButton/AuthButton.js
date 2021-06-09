86775ef2cfaff200d386179efa7049cf3320d6b2
Lany75 <mlanie.parry@gmail.com>
2021-06-09 15:09:04 +0200

    F2 - Modification composant AuthButton

--- monbartender/src/components/authButton/AuthButton.js
+++ monbartender/src/components/authButton/AuthButton.js
@@ -1,6 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useHistory } from 'react-router';
-import { BsPeopleCircle } from "react-icons/bs";
+import { Link } from 'react-router-dom';
+import { IconButton, Menu, MenuItem } from "@material-ui/core";
+import { AccountCircle } from "@material-ui/icons";
 
 import { AuthContext } from '../../context/authContext';
 import { BarContext } from '../../context/barContext';
@@ -10,12 +12,23 @@ import './AuthButtonDesktop.css';
 
 const AuthButton = () => {
   let { user, signOut, setAccessToken } = useContext(AuthContext);
-  const { setBar } = useContext(BarContext);
-  const userName = user ? (user.displayName.split(' '))[0] : 'Visiteur';
+  const { bar, setBar } = useContext(BarContext);
+  const [anchorEl, setAnchorEl] = useState(null);
 
+  const userName = user && (user.displayName.split(' '))[0];
+  const open = Boolean(anchorEl);
   let history = useHistory();
 
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const connexion = () => {
+    setAnchorEl(null);
     history.push('/connexion');
   }
 
@@ -24,14 +37,75 @@ const AuthButton = () => {
     setBar(null);
     setAccessToken(null);
     user = null;
+    setAnchorEl(null);
     history.push("/");
   }
 
   return (
     <div className='auth'>
-      <div className='auth-button' onClick={user ? deconnexion : connexion}><BsPeopleCircle /></div>
-      <div className='auth-name'>{userName}</div>
-    </div>)
+      <IconButton
+        aria-label="account of current user"
+        aria-controls="menu-appbar"
+        aria-haspopup="true"
+        onClick={handleMenu}
+        color="inherit"
+      >
+        <AccountCircle />
+        {user && (
+          <div className='auth-name'>{userName}</div>
+        )}
+      </IconButton>
+      <Menu
+        id="auth-appbar"
+        anchorEl={anchorEl}
+        keepMounted
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        open={open}
+        onClose={handleClose}
+      >
+        {user ? (
+          <div>
+            <MenuItem onClick={handleClose}>Mon profil</MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Link
+                to='/monbar'
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                Mon bar
+              </Link>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>Mes favoris</MenuItem>
+            {bar && bar.droits === true && (
+              <MenuItem onClick={handleClose}>
+                <Link
+                  to='/gestion'
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Gestion
+                </Link>
+              </MenuItem>
+            )}
+            <MenuItem onClick={deconnexion}>Déconnexion</MenuItem>
+          </div>
+        ) : (
+          <div>
+            <MenuItem onClick={handleClose}>S'inscrire</MenuItem>
+            <MenuItem onClick={connexion}>
+              <Link
+                to='/connexion'
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                Se connecter
+              </Link>
+            </MenuItem>
+          </div>
+        )}
+      </Menu>
+    </div>
+  )
 }
 
 export default AuthButton;
