30dce9ca06b9852d87065d020dedc55adf22cfa0
Lany75 <mlanie.parry@gmail.com>
2021-06-10 12:36:36 +0200

    F11 - Création des tests du composant Header

--- monbartender/src/tests/components/App.test.js
+++ monbartender/src/tests/components/App.test.js
@@ -7,7 +7,9 @@ import Header from '../../components/header/Header';
 import Main from '../../components/main/Main';
 
 describe('<App />', () => {
+
   const app = shallow(<App />);
+
   it('should contain a div witch className is container', () => {
     expect(app.find('.container')).to.have.length(1);
   });
