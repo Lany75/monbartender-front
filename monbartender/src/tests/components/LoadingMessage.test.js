1ff8a204e5cfbe72051dbd9805e76c284e2d2aaa
Lany75 <mlanie.parry@gmail.com>
2021-06-22 17:19:14 +0200

    F33 - Create tests for LoadingMessage component

---
+++ monbartender/src/tests/components/LoadingMessage.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
+import LoadingMessage from '../../components/loadingMessage/LoadingMessage';
+
+describe('<LoadingMessage />', () => {
+  const loadingMessage = shallow(<LoadingMessage message='Loading message ...' />);
+
+  it('should contain a div witch className is loading-message', () => {
+    expect(loadingMessage.find('.loading-message')).to.have.length(1);
+  })
+
+  it('should contain the text "Loading message ..."', () => {
+    expect(loadingMessage.find('.loading-message').text()).to.be.equal('Loading message ...')
+  })
+})
