e11f511bee00a6c43ec173af104e492c99e8119e
Lany75 <mlanie.parry@gmail.com>
2021-07-16 16:17:53 +0200

    F50 - Rearrange management of ingredients categories

---
+++ monbartender/src/tests/components/ManageCategories.test.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
+import ManageCategories from '../../components/manageCategories/ManageCategories';
+import IngredientCategoryAdd from '../../components/ingredientCategoryAdd/IngredientCategoryAdd';
+
+describe('<ManageCategories />', () => {
+  const manageCategories = shallow(<ManageCategories />);
+  const divManageCategories = manageCategories.find('div.manage-categories');
+
+  it('should contain a div witch clasSName is manage-categories', () => {
+    expect(divManageCategories).to.have.length(1);
+  })
+
+  it('should contain a h4 tag witch text="LES CATEGORIES D\'INGREDIENTS"', () => {
+    expect(divManageCategories.find('h4')).to.have.length(1);
+    expect(divManageCategories.find('h4').text()).to.be.equal("LES CATEGORIES D'INGREDIENTS");
+  })
+
+  it('should contain an IngredientCategoryList component', () => {
+    expect(divManageCategories.find('IngredientCategoryList')).to.have.length(1);
+  })
+
+  it.skip('should contain an IngredientCategoryChange component', () => {
+    expect(divManageCategories.find('IngredientCategoryChange')).to.have.length(1);
+  })
+
+  it('should contain an IngredientCategoryAdd component', () => {
+    expect(divManageCategories).to.contain(<IngredientCategoryAdd />);
+  })
+})
--- monbartender/src/tests/components/ManagementPage.test.js
+++ monbartender/src/tests/components/ManagementPage.test.js
@@ -5,6 +5,10 @@ import { shallow } from 'enzyme';
 import { Paper, Tabs, Tab } from '@material-ui/core';
 
 import ManagementPage from '../../components/managementPage/ManagementPage';
+import GestionCocktailMoment from '../../components/gestionCocktailMoment/GestionCocktailMoment';
+import GestionCocktails from '../../components/gestionCocktails/GestionCocktails';
+import ManageIngredients from '../../components/manageIngredients/ManageIngredients';
+
 
 describe('<ManagementPage />', () => {
   const managementPage = shallow(<ManagementPage />);
@@ -29,14 +33,66 @@ describe('<ManagementPage />', () => {
     expect(paperTabs.props().hasOwnProperty('onChange')).to.be.true;
   })
 
-  it('should contain 5 Tab components witch id is management-tab-label', () => {
-    expect(paperTabs.find(Tab)).to.have.length(5);
-    paperTabs.find(Tab).map(pt => {
+  const tabComponents = paperTabs.find(Tab);
+
+  it('should contain 6 Tab components witch id is management-tab-label', () => {
+    expect(tabComponents).to.have.length(6);
+    tabComponents.find(Tab).map(pt => {
       expect(pt.props()).to.have.property('id', 'management-tab-label');
     })
   })
 
-  it('should contain 5 TabPanel components', () => {
-    expect(managementPage.find('TabPanel')).to.have.length(5);
+  it('the first Tab component should have label="Des Cocktails du Moment"', () => {
+    expect(tabComponents.get(0).props.label).to.be.equal('Des Cocktails du Moment');
+  })
+
+  it('the second Tab component should have label="Des Cocktails"', () => {
+    expect(tabComponents.get(1).props.label).to.be.equal('Des Cocktails');
+  })
+
+  it('the third Tab component should have label="Des Ingrédients"', () => {
+    expect(tabComponents.get(2).props.label).to.be.equal('Des Ingrédients');
+  })
+
+  it('the fourth Tab component should have label="Des Catégories d\'Ingrédients"', () => {
+    expect(tabComponents.get(3).props.label).to.be.equal("Des Catégories d'Ingrédients");
+  })
+
+  it('the fifth Tab component should have label="Des Verres"', () => {
+    expect(tabComponents.get(4).props.label).to.be.equal('Des Verres');
+  })
+
+  it('the sixth Tab component should have label="Des Admin"', () => {
+    expect(tabComponents.get(5).props.label).to.be.equal('Des Admin');
+  })
+
+  const tabPanelComponents = managementPage.find('TabPanel');
+
+  it('should contain 6 TabPanel components', () => {
+    expect(tabPanelComponents).to.have.length(6);
+  })
+
+  it.skip('the first TabPanel component should contain a GestionCocktailMoment component', () => {
+    expect(tabPanelComponents).to.contain(<GestionCocktailMoment />);
+  })
+
+  it.skip('the second TabPanel component should contain a GestionCocktails component', () => {
+    expect(tabPanelComponents).to.contain(<GestionCocktails />);
+  })
+
+  it.skip('the third TabPanel component should contain a ManageIngredients component', () => {
+    expect(tabPanelComponents).to.contain(<ManageIngredients />);
+  })
+
+  it.skip('the fourth TabPanel component should contain a ManageCategories component', () => {
+    expect(tabPanelComponents).to.contain(<ManageCategories />);
+  })
+
+  it.skip('the fifth TabPanel component should contain a GestionVerres component', () => {
+    expect(tabPanelComponents).to.contain(<GestionVerres />);
+  })
+
+  it.skip('the sixth TabPanel component should contain a GestionAdmin component', () => {
+    expect(tabPanelComponents).to.contain(<GestionAdmin />);
   })
 })
