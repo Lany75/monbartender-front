d862f8b7cfe470cc2a110562c233721286a32089
Lany75 <mlanie.parry@gmail.com>
2021-07-20 17:21:59 +0200

    F51 - Modify IngredientCategoryList for deleting category

--- monbartender/src/components/ingredientCategoryList/IngredientCategoryList.js
+++ monbartender/src/components/ingredientCategoryList/IngredientCategoryList.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import Axios from "axios";
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TablePagination } from '@material-ui/core';
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+
+import apiBaseURL from "../../env";
 
 import LoadingMessage from '../loadingMessage/LoadingMessage';
 import { IngredientContext } from '../../context/ingredientContext';
@@ -7,7 +11,7 @@ import { IngredientContext } from '../../context/ingredientContext';
 import './IngredientCategoryList.css';
 
 const IngredientCategoryList = ({ setCategoryClicked }) => {
-  const { listeCategoriesIngredients } = React.useContext(IngredientContext);
+  const { listeCategoriesIngredients, setListeCategoriesIngredients } = React.useContext(IngredientContext);
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
@@ -20,6 +24,18 @@ const IngredientCategoryList = ({ setCategoryClicked }) => {
     setPage(0);
   };
 
+  const deleteCategory = (categoryId) => {
+    Axios.delete(`${apiBaseURL}/api/v2/ingredients/category/${categoryId}`)
+      .then(reponse => {
+        setListeCategoriesIngredients(reponse.data);
+        setCategoryClicked(null);
+      })
+      .catch(error => {
+        console.log("vous avez une erreur : ", error);
+      });
+
+  }
+
   return (
     <>
       {listeCategoriesIngredients ? (
@@ -30,14 +46,16 @@ const IngredientCategoryList = ({ setCategoryClicked }) => {
                 <TableRow>
                   <TableCell>ID</TableCell>
                   <TableCell className='table-cell-head' align='right'>NOM</TableCell>
+                  <TableCell align='right'></TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
                 {listeCategoriesIngredients.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
                   return (
-                    <TableRow hover role="checkbox" tabIndex={-1} key={row.id} onClick={() => setCategoryClicked(row)}>
-                      <TableCell>{row.id.split('-')[0]}</TableCell>
-                      <TableCell align='right'>{row.nom}</TableCell>
+                    <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                      <TableCell onClick={() => setCategoryClicked(row)}>{row.id.split('-')[0]}</TableCell>
+                      <TableCell align='right' onClick={() => setCategoryClicked(row)}>{row.nom}</TableCell>
+                      <TableCell align='right'><DeleteForeverIcon onClick={() => deleteCategory(row.id)} /></TableCell>
                     </TableRow>
                   );
                 })}
