8f10aada61d9871cc8f649a950ae664ec15e7667
Lany75 <mlanie.parry@gmail.com>
2021-08-25 14:45:18 +0200

    F71 - Delete ingredients from user's bar

--- monbartender/src/components/userBarIngredientList/UserBarIngredientList.js
+++ monbartender/src/components/userBarIngredientList/UserBarIngredientList.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import Axios from "axios";
 import { DataGrid } from '@material-ui/data-grid';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, useMediaQuery } from "@material-ui/core";
+
+import apiBaseURL from "../../env";
 
 import { BarContext } from '../../context/barContext';
 import './UserBarIngredientList.css'
+import { AuthContext } from '../../context/authContext';
 
 const UserBarIngredientList = () => {
-  const { bar } = React.useContext(BarContext);
+  const { bar, setBar } = React.useContext(BarContext);
+  const { accessToken } = React.useContext(AuthContext);
   const [ingredients, setIngredients] = React.useState([]);
   const [pageSize, setPageSize] = React.useState(5);
+  const [selectedRow, setSelectedRow] = React.useState([]);
+  const [open, setOpen] = React.useState(false);
+  const [message, setMessage] = React.useState('');
   const desktop = useMediaQuery('(min-width:769px)');
 
   const columns = [
@@ -23,6 +31,39 @@ const UserBarIngredientList = () => {
       width: desktop ? 300 : 150,
     }
   ];
+
+  const selectRow = (event) => {
+    setSelectedRow(event);
+    setMessage('');
+  }
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const deleteIngredients = () => {
+    if (selectedRow.length > 0) handleClickOpen();
+    else setMessage('Aucun ingrédient sélectionné')
+  }
+
+  const confirmDeletion = () => {
+    Axios.delete(`${apiBaseURL}/api/v2/barsIgredients/`, {
+      headers: {
+        authorization: accessToken
+      },
+      data: { deletedIngredients: selectedRow }
+    })
+      .then(reponse => {
+        setBar(reponse.data);
+      })
+      .catch(error => {
+        console.log("vous avez une erreur : ", error);
+      });
+
+    handleClose();
+  }
 
   React.useEffect(() => {
     const rows = [];
@@ -44,9 +85,40 @@ const UserBarIngredientList = () => {
           rowsPerPageOptions={[5, 10, 20]}
           pagination
           checkboxSelection
-          disableSelectionOnClick
+          onSelectionModelChange={selectRow}
         />
       </div>
+      <div className='delete-ingredient-bar'>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={deleteIngredients}
+        >
+          Supprimer les ingrédients
+        </Button>
+        <div className='message'>{message}</div>
+      </div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='alert-dialog-title'
+        aria-describedby='alert-dialog-description'
+      >
+        <DialogTitle id='alert-dialog-title'>{'Confirmer la suppression des ingrédients'}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id='alert-dialog-description'>
+            Etes vous sûr de vouloir supprimer ces ingrédients de votre bar ?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color='primary'>
+            Annuler
+          </Button>
+          <Button onClick={confirmDeletion} color='primary' autoFocus>
+            Confirmer
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   )
 }
