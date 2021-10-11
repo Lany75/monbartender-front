15ebe2e6ac6bb7deac988f062f5df27ed4fcc07e
Lany75 <mlanie.parry@gmail.com>
2021-10-11 15:08:53 +0200

    F44 - Create CocktailAddGlass component

--- monbartender/src/components/cocktailAdd/CocktailAdd.js
+++ monbartender/src/components/cocktailAdd/CocktailAdd.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import { InputLabel, MenuItem, FormControl, Select, Button } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import './CocktailAdd.css';
-import { VerreContext } from '../../context/verreContext';
 import CocktailAddNameType from '../cocktailAddNameType/CocktailAddNameType';
 import CocktailAddPhoto from '../cocktailAddPhoto/CocktailAddPhoto';
+import CocktailAddGlass from '../cocktailAddGlass/CocktailAddGlass';
 
 const CocktailAdd = () => {
-  const { listeVerres } = React.useContext(VerreContext);
-  const [chosenGlass, setChosenGlass] = React.useState('');
-
-  const handleChangeChosenGlass = (event) => {
-    setChosenGlass(event.target.value);
-  };
-
   const addCocktail = (event) => {
     event.preventDefault();
-    console.log('verre : ', chosenGlass);
   }
 
   return (
@@ -24,26 +16,7 @@ const CocktailAdd = () => {
       <form className='form-cocktail-add' onSubmit={addCocktail}>
         <CocktailAddNameType />
         <CocktailAddPhoto />
-
-        <FormControl variant='outlined' >
-          <InputLabel id='label-glass'>Verre</InputLabel>
-          <Select
-            className='form-control-select'
-            labelId='select-glass'
-            id='select-glass'
-            value={chosenGlass}
-            onChange={handleChangeChosenGlass}
-            label='Verre'
-            style={{ width: 220 }}
-            required
-          >
-            {listeVerres && listeVerres.map(glass => {
-              return (
-                <MenuItem value={glass.nom} key={glass.id}>{glass.nom}</MenuItem>
-              )
-            })}
-          </Select>
-        </FormControl>
+        <CocktailAddGlass />
 
         <Button
           style={{ marginTop: 50 }}
---
+++ monbartender/src/components/cocktailAddGlass/CocktailAddGlass.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+import { VerreContext } from "../../context/verreContext";
+
+const CocktailAddGlass = () => {
+  const { listeVerres } = React.useContext(VerreContext);
+  const [chosenGlass, setChosenGlass] = React.useState('');
+
+  const handleChangeChosenGlass = (event) => {
+    setChosenGlass(event.target.value);
+  };
+
+  return (
+    <FormControl variant='outlined' >
+      <InputLabel id='label-glass'>Verre</InputLabel>
+      <Select
+        className='form-control-select'
+        labelId='select-glass'
+        id='select-glass'
+        value={chosenGlass}
+        onChange={handleChangeChosenGlass}
+        label='Verre'
+        style={{ width: 220 }}
+        required
+      >
+        {listeVerres && listeVerres.map(glass => {
+          return (
+            <MenuItem value={glass.nom} key={glass.id}>{glass.nom}</MenuItem>
+          )
+        })}
+      </Select>
+    </FormControl>
+  )
+}
+
+export default CocktailAddGlass;
