544b4e799e63e6c61028e34e9b63cd8ad7d4d0c4
Lany75 <mlanie.parry@gmail.com>
2021-11-03 19:21:16 +0100

    F44 - ManageCocktail, delete cocktail

--- monbartender/src/components/manageCocktails/ManageCocktails.js
+++ monbartender/src/components/manageCocktails/ManageCocktails.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { useHistory } from 'react-router';
+import Axios from "axios";
 import { DataGrid } from '@material-ui/data-grid';
-import { Button, /*Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField,*/ useMediaQuery } from '@material-ui/core';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, useMediaQuery } from '@material-ui/core';
+
+import { refStorage } from "../../firebaseConfig";
+import apiBaseURL from "../../env";
 
 import { CocktailContext } from '../../context/cocktailContext';
+import { AuthContext } from '../../context/authContext';
 import ImageCocktail from '../imageCocktail/ImageCocktail';
 import './ManageCocktails.css';
 
 const ManageCocktails = () => {
-  const { listeCocktails } = React.useContext(CocktailContext);
+  const { accessToken } = React.useContext(AuthContext);
+  const { listeCocktails, setListeCocktails } = React.useContext(CocktailContext);
   let history = useHistory();
   const [pageSize, setPageSize] = React.useState(5);
+  const [selectedRow, setSelectedRow] = React.useState([]);
   const desktop = useMediaQuery('(min-width:769px)');
+  const [openDeleteCocktailDialog, setOpenDeleteCocktailDialog] = React.useState(false);
 
   const columns = [
     {
@@ -43,6 +51,57 @@ const ManageCocktails = () => {
     history.push('/gestion/nouveau-cocktail/');
   }
 
+  const selectRow = (event) => {
+    setSelectedRow(event);
+  }
+
+  const deleteCocktails = () => {
+    if (selectedRow.length > 0) handleOpenDeleteCocktailDialog();
+  }
+
+  const handleOpenDeleteCocktailDialog = () => {
+    setOpenDeleteCocktailDialog(true);
+  };
+
+  const handleCloseDeleteCocktailDialog = () => {
+    setOpenDeleteCocktailDialog(false);
+  };
+
+  const deleteImageOnFirebase = refTab => {
+    refTab.forEach(ref => {
+      if (ref !== 'img_cocktail/noImageFound.jpg') {
+        // initialisation de la référence de l'image
+        const imgRef = refStorage.child(ref);
+
+        imgRef.delete().then(() => {
+          console.log('photo supprimé de firebase')
+        })
+          .catch((err) => {
+            console.log("error deleting file", err);
+          });
+      }
+    })
+  }
+
+  const confirmDeletion = () => {
+    Axios.delete(`${apiBaseURL}/api/v2/cocktails/`,
+      {
+        headers: {
+          authorization: accessToken
+        },
+        data: { deletedCocktails: selectedRow }
+      })
+      .then(reponse => {
+        deleteImageOnFirebase(reponse.data.images);
+        setListeCocktails(reponse.data.cocktails);
+      })
+      .catch(error => {
+        console.log("vous avez une erreur : ", error);
+      });
+
+    handleCloseDeleteCocktailDialog();
+  }
+
   return (
     <div className='manage-cocktails'>
       <h4>LES COCKTAILS</h4>
@@ -56,7 +115,7 @@ const ManageCocktails = () => {
           pagination
           checkboxSelection
           disableSelectionOnClick
-        //onSelectionModelChange={selectRow}
+          onSelectionModelChange={selectRow}
         //onCellClick={handleClickOpenModifyGlassDialog}
         />
       </div>
@@ -65,7 +124,7 @@ const ManageCocktails = () => {
         <Button
           variant="contained"
           color="primary"
-        //onClick={deleteCocktails}
+          onClick={deleteCocktails}
         >
           Supprimer les cocktails
         </Button>
@@ -78,6 +137,29 @@ const ManageCocktails = () => {
           +
         </Button>
       </div>
+
+
+      <Dialog
+        open={openDeleteCocktailDialog}
+        onClose={handleCloseDeleteCocktailDialog}
+        aria-labelledby='alert-dialog-title'
+        aria-describedby='alert-dialog-description'
+      >
+        <DialogTitle id='alert-dialog-title'>Confirmer la suppression des cocktails</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Etes vous sûr de vouloir supprimer ces cocktails définitivement ?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDeleteCocktailDialog} color='primary'>
+            Annuler
+          </Button>
+          <Button onClick={confirmDeletion} color='primary' autoFocus>
+            Confirmer
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   )
 }
